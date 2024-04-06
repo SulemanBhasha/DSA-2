80dd1831c73fb5e009b4fe03fd00e07067facd20
Anfal12345 <sulemanbhashadodmni@gmail.com>
2024-04-07 02:10:00 +0530

    Implent_Binray_tree

---
+++ Binary_Tree/Implent_Binray_tree.java
@@ -0,0 +1,40 @@
+public class Implent_Binray_tree {
+
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node createTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = createTree(nodes);
+            newNode.right = createTree(nodes);
+            return newNode;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree bt = new BinaryTree();
+        Node node = bt.createTree(nodes);
+        System.out.println(node.data);
+        System.out.println(node.left.data);
+    }
+
+}
